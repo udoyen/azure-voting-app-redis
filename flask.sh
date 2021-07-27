61017550f31efc3a8047481d0c2aace616b19bf8
george <datameshprojects@gmail.com>
2021-07-27 16:49:29 +0100

    updated flask to be independent of cloud-init workflow

--- flask.sh
+++ flask.sh
@@ -3,19 +3,21 @@
 set -e
 
 apt-get update
-echo " "
+
 echo "Installing requirements ..."
-echo " " 
+ 
 pip3 install -r /home/udacityadmin/azure-voting-app-redis/requirements.txt
-echo " "
+
 echo "Finished installing requirements"
-echo " "
+
 echo "Updating ownership of the flask app folder ..."
-echo " "
+
 sudo chown -R udacityadmin:udacityadmin /home/udacityadmin/azure-voting-app-redis
-echo " "
+
 echo "Finished folder ownership update"
-echo " "
+
 echo "Starting flask app ..."
-python3 /home/udacityadmin/azure-voting-app-redis/azure-vote/main.py || echo "Flask start failed!"
-echo " "
+
+python3 /home/udacityadmin/azure-voting-app-redis/azure-vote/main.py && echo "Flask app started" || echo "Flask app start failed!"
+
+
