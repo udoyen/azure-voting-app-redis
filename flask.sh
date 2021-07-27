bdc0ee92493f07b9cbc7fce7f7c8c41aee0ddeaa
george udosen <george@DESKTOP-OR7D47V.localdomain>
2021-07-27 17:38:17 +0100

    update flask file

--- flask.sh
+++ flask.sh
@@ -8,7 +8,7 @@ echo "Installing requirements ..."
 
 echo "Setting up venv"
 
-sudo pip3 install virtualenv
+sudo -H pip3 install virtualenv
  
 
 echo "Entering target folder ... "
@@ -25,7 +25,7 @@ source /home/udacityadmin/azure-voting-app-redis/venv/bin/activate
 
 echo "Installing python requirements"
 
-pip3 install --ignore-installed pyasn1-modules==0.2.1 -r /home/udacityadmin/azure-voting-app-redis/requirements.txt
+pip3 install --ignore-installed pyasn1-modules -r /home/udacityadmin/azure-voting-app-redis/requirements.txt
 
 echo "Updating ownership of the flask app folder ..."
 
